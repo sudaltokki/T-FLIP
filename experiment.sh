f1753c0165b7cd7c673ecc330ce450192f8f99d8
Yerim <137975664+mireyerim@users.noreply.github.com>
2024-08-16 11:21:03 +0900

    flip-kd
    added swin-t

---
+++ experiment.sh
@@ -0,0 +1,21 @@
+torchrun --nproc_per_node 1 -m \
+    --master_addr=127.0.0.2 --master_port=28640 \
+    student.main_kd \
+    --t_model ViT-B-16 \
+    --model ViT-T-16 \
+    --t-model-checkpoint "/home/jiwon/FLIP-KD-Smod/student/t_ckpt/msu_flip_mcl.pth.tar"\
+    --config M \
+    --op_dir ckpt \
+    --report_logger_path log \
+    --root "/nas/dataset/FLIP_Dataset/MCIO/frame/"\
+    --dataroot "/nas/dataset/FLIP_Dataset/MCIO/txt/"\
+    --epochs 300 \
+    --batch_size 8 \
+    --t_batch_size 30 \
+    --lr=0.000001 \
+    --alpha_ckd_loss 0. \
+    --alpha_fd_loss 0. \
+    --alpha_affinity_loss 0 \
+    --alpha_icl_loss 0.  \
+    --name test  \
+    --swin True
